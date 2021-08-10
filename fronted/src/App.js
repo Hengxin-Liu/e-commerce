c52c54020b9ebbd21e7fd94747013998d412922f
Merry Cone <caikaige@hotmail.com>
2021-08-10 19:46:02 +1000

    show product detail

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import data from './data';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import Product from './component/Product';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import ProductScreen from './component/ProductScreen';
+import HomeScreen from './component/HomeScreen';
 
 
 function App() {
-  return (
-    <div className="grid-container">
-    <header className="row">
-        <div>  
-        <a href="/"  className="brand">amazona</a>
-        </div>
-         <div>
-         <a href="/cart">Cart</a>
-         <a href="/sigin">Sign In</a>
-         </div>
-     </header>
-     <main>
-         <div>
-         <div className="row center">
-         {data.products.map(product =>(
-          <Product key ={product._id} product={product} />
-        ))}
-      </div>
-     </div>
-</main>
-    <footer className="row center"> All right reserved</footer>
- </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <Link to="/" className="brand">amazona</Link>
+                    </div>
+                    <div>
+                        <Link to="/cart">Cart</Link>
+                        <Link to="/sigin">Sign In</Link>
+                    </div>
+                </header>
+                <main>
+                    <Route path="/product/:id" component={ProductScreen} />
+                    <Route path="/" component={HomeScreen} exact />
+
+                </main>
+                <footer className="row center"> All right reserved</footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
