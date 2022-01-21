e1729cb9cb77f9badf4157ce4435503645a3f950
Harry <liu2332076156@hotmail.com>
2022-01-21 12:26:01 +1100

    depoly to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path';
+import path, { dirname } from 'path';
 import userRouter from './router/userRouter.js';
 import productRouter from './router/productRouter.js';
 import orderRouter from './router/orderRouter.js';
@@ -30,9 +30,14 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 //Serve static content for the app from the “uploads” directory in the application directory:
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/',(req,res)=>{
-    res.send('Server is ready');
-});
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*',(req, res) => 
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+ );
+ // app.get('/', (req, res) => {
+ //   res.send('Server is ready');
+ // });
 app.use((err,req,res,next) =>{
     res.status(500).send({message:err.message});
 });
